d0bc589720743fc538430a32271bfb79e8f3c326
kvlinden <kvlinden@calvin.edu>
2018-09-20 08:05:32 -0400

    added JSHint/JSLint demo; cleaned up JS syntax...

--- unit03-node/server.js
+++ unit03-node/server.js
@@ -3,28 +3,28 @@
  *    node script.js
  *    npm start
  */
+"use strict";
 
-const http = require('http');
-const http_status = require('http-status-codes');
+const http = require("http");
+const http_status = require("http-status-codes");
 
 const HOST = "127.0.0.1";
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    'use strict';
-    if (req.url == '/hello') {
-        res.writeHead(http_status.OK, {'Content-Type': 'text/plain'});
-        res.end('Hello, Node.js!');
+    if (req.url === "/hello") {
+        res.writeHead(http_status.OK, {"Content-Type": "text/plain"});
+        res.end("Hello, Node.js!");
     } else {
-        res.writeHead(http_status.NOT_FOUND, {'Content-Type': 'text/plain'});
+        res.writeHead(http_status.NOT_FOUND, {"Content-Type": "text/plain"});
         res.end();
     }
 });
 
 server.listen(PORT, HOST, () => {
-    console.log('Server running on ' + HOST + ':' + PORT + '.');
+    console.log("Server running on " + HOST + ":" + PORT + ".");
 });
 
 // Load/Invoke a function from a custom module.
-let my_module = require('./module');
+let my_module = require("./module");
 console.log(my_module.my_function());
